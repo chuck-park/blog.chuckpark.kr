9fb2376664fc6749596442cea56ab8441de87a3b
Chuck Park <chuck.m.park@gmail.com>
2026-03-17 11:56:40 +0900

    refactor: activate inkframe theme assets

--- banner.js
+++ banner.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
 const pkg = require("./package.json");
-const filename = "assets/js/main.min.js";
+const filename = process.argv[2] || "assets/js/main.min.js";
 const script = fs.readFileSync(filename);
 const padStart = str => ("0" + str).slice(-2);
 const dateObj = new Date();
 const date = `${dateObj.getFullYear()}-${padStart(
   dateObj.getMonth() + 1
 )}-${padStart(dateObj.getDate())}`;
+const themeName = filename.includes("inkframe") ? "Inkframe" : "Minimal Mistakes Jekyll Theme";
 const banner = `/*!
- * Minimal Mistakes Jekyll Theme ${pkg.version} by ${pkg.author}
+ * ${themeName} ${pkg.version} by ${pkg.author}
  * Copyright 2013-${dateObj.getFullYear()} Michael Rose - mademistakes.com | @mmistakes
  * Licensed under ${pkg.license}
  */
